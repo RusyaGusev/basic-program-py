679e043ee6bbf340be80b1838f816cfd26b6d57f
RusyaGusev <107546184+RusyaGusev@users.noreply.github.com>
2023-02-02 21:18:06 +1300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 
+
 def main():
     print("Hello welcome to our new messenger! How can I call you?")
     username = input("Enter username: ")
@@ -6,18 +7,23 @@ def main():
     answer = input()
     gen_answer = answer.lower()
     while gen_answer != 'yes' and gen_answer != 'no':
-        print("What do you want to say?")
-        message = input()
-        print(username + " said: " + message)
+        print("Answer may only be 'Yes' or 'No'")
+        print("Hello " + username + ", Do you want to say something?")
+        answer = input()
+        gen_answer = answer.lower()
     while gen_answer != 'no':
         print("What do you want to say?")
         message = input()
         print(username + " said: " + message)
-    while gen_answer != 'yes':
-        print("What do you want to say?")
-        message = input()
-        print(username + " said: " + message)
-    leave = input("Press enter to exit")
+        print("Hello " + username + " Do you want to say something?")
+        answer = input()
+        gen_answer = answer.lower()
+    while gen_answer != 'yes' and gen_answer != 'no':
+        print("Answer may only be 'Yes' or 'No'")
+        print("Hello " + username + " Do you want to say something?")
+        answer = input()
+        gen_answer = answer.lower()
+    leave = input("Press enter to leave.")
 if __name__ == '__main__':
     main()
 
