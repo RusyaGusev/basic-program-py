71514716013cd50a99c9f24255df943fcff340ce
RusyaGusev <107546184+RusyaGusev@users.noreply.github.com>
2023-02-04 14:27:13 +1300

    Update test.py

--- test.py
+++ test.py
@@ -18,10 +18,10 @@ def main():
         print("Hello " + username + " Do you want to say something?")
         answer = input()
         gen_answer = answer.lower()
-    while gen_answer != 'yes' and gen_answer != 'no':
-        print("Answer may only be 'Yes' or 'No'")
-        print("Hello " + username + " Do you want to say something?")
-        answer = input()
+        while gen_answer != 'yes' and gen_answer != 'no':
+            print("Answer may only be 'Yes' or 'No'")
+            print("Hello " + username + " Do you want to say something?")
+            answer = input()
         gen_answer = answer.lower()
     leave = input("Press enter to leave.")
 if __name__ == '__main__':
